973bf098a9bd69935e36709bdd4543a7440dd530
George-Hachem <78202573+George-H12@users.noreply.github.com>
2023-05-17 18:27:33 -0400

    Initial Commit

---
+++ youBetterRest/youBetterRestApp.swift
@@ -0,0 +1,17 @@
+//
+//  youBetterRestApp.swift
+//  youBetterRest
+//
+//  Created by George on 5/17/23.
+//
+
+import SwiftUI
+
+@main
+struct youBetterRestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
